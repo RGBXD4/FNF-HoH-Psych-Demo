62c63f519ad8a6f2fea63d769609fc9072da33dd
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-12-14 18:13:51 +0800

    Update CopyState.hx

--- source/mobile/states/CopyState.hx
+++ source/mobile/states/CopyState.hx
@@ -188,8 +188,7 @@ class CopyState extends MusicBeatState
 		locatedFiles = OpenflAssets.list();
 		// removes unwanted assets
 		var assets = locatedFiles.filter(folder -> folder.startsWith('assets/'));
-		var mods = locatedFiles.filter(folder -> folder.startsWith('mods/'));
-		locatedFiles = assets.concat(mods);
+		locatedFiles = assets.concat();
 
 		var filesToRemove:Array<String> = [];
 		for (file in locatedFiles)
